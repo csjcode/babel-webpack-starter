b6b1ed5673617ca10e823af0451f241ada8d9a9c
Chris <christophersstjohn@gmail.com>
2017-12-03 22:48:12 -0800

    Sample React snippet shortcuts

---
+++ index.js
@@ -0,0 +1,32 @@
+// rrcp conc cwm est
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+props
+class componentName extends Component {
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            
+        }
+    }
+
+    static propTypes = {
+
+    }
+
+    
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default componentName;
